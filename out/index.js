362967299992bcff1ba800840e426a021f1b93fc
Luana Souza <128157430+luanaapms@users.noreply.github.com>
2025-04-10 21:22:13 -0300

    adicionando módulo

---
+++ out/index.js
@@ -0,0 +1,28 @@
+export class Somador {
+    constructor() {
+        this.calcular = (n1, n2) => {
+            return n1 + n2;
+        };
+    }
+}
+export class Subtrator {
+    constructor() {
+        this.calcular = (n1, n2) => {
+            return n1 - n2;
+        };
+    }
+}
+export class Multiplicador {
+    constructor() {
+        this.calcular = (n1, n2) => {
+            return n1 * n2;
+        };
+    }
+}
+export class Divisor {
+    constructor() {
+        this.calcular = (n1, n2) => {
+            return n1 / n2;
+        };
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export class Somador {
+    calcular = (n1: number, n2: number) => {
+        return n1 + n2
+    } 
+}
+
+export class Subtrator {
+    calcular = (n1: number, n2: number) => {
+        return n1 - n2
+    }
+}
+export class Multiplicador {
+    calcular = (n1: number, n2: number) => {
+        return n1 * n2
+    }
+}
+export class Divisor {
+    calcular = (n1: number, n2: number) => {
+        return n1 / n2
+    }
+}
